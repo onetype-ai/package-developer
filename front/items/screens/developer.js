5e84d53418d1de686b4b1c26a579034edafdeb7a
Dejan Tomic <hi@iamdejan.com>
2026-07-11 18:25:17 +0200

    developer: mode screens with deep links

--- front/items/screens/developer.js
+++ front/items/screens/developer.js
@@ -6,4 +6,30 @@ onetype.AddonReady('ui.screens', (screens) =>
 		app: 'developer',
 		metadata: { addon: 'developer' }
 	});
+
+	screens.Item({
+		id: 'developer.elements',
+		route: ['/developer/elements', '/developer/elements/:id'],
+		app: 'developer',
+		mode: 'elements',
+		params: { id: 'developer_elements_selected' },
+		metadata: { addon: 'developer' }
+	});
+
+	screens.Item({
+		id: 'developer.addons',
+		route: ['/developer/addons', '/developer/addons/:id'],
+		app: 'developer',
+		mode: 'addons',
+		params: { id: 'developer_addons_selected' },
+		metadata: { addon: 'developer' }
+	});
+
+	screens.Item({
+		id: 'developer.styles',
+		route: '/developer/styles',
+		app: 'developer',
+		mode: 'styles',
+		metadata: { addon: 'developer' }
+	});
 });
